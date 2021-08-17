5a2ca246b3b9fe0632dced3e7b3a2fcab55cd86e
romikgrg <86762133+romikgrg@users.noreply.github.com>
2021-08-17 21:10:20 +0545

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,30 @@
+<?php
+include "connection.php";
+?>
+<html>
+<title>
+    Hamro Library
+</title>
+<head>
+<link rel="stylesheet" type="text/css" href="nav.css">
+<style>
+    
+</style>
+</head>
+<body>
+<div class="navbar">
+    <label class="logo"><a class = "b" href="../home.php"><span>H</span>amro Library</a></label>
+    <ul class="nav">
+<li><a  class="active" href="../home.php">Home</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
+<li><a  href="#about">About Us</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
+<li><a  href="user/login.php">Login/Signup</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
+<li><a  href="admin/login.php">Admin</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
+<li><a  href="#contact">Contact</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
+</ul>
+
+</div>
+<br>
+
+
+</body>
+</html>
